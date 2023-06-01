e7abb8b4faab20189aa9f831fc1523048600d7e3
Pintu Das <impin2rex@gmail.com>
2023-06-01 18:39:46 +0530

    unknown instruction fallback added if instruction data failed to parse due to proper IDL error

--- src/parsers.ts
+++ src/parsers.ts
@@ -708,9 +708,24 @@ export class SolanaParser {
 		if (!this.instructionParsers.has(instruction.programId.toBase58())) {
 			return this.buildUnknownParsedInstruction(instruction.programId, instruction.keys, instruction.data);
 		} else {
-			const parser = this.instructionParsers.get(instruction.programId.toBase58()) as ParserFunction<I, IxName>;
+			try {
+				const parser = this.instructionParsers.get(instruction.programId.toBase58()) as ParserFunction<I, IxName>;
 
-			return parser(instruction);
+				return parser(instruction);
+			} catch (error) {
+				// eslint-disable-next-line no-console
+				console.error("Parser does not matching the instruction args", {
+					programId: instruction.programId.toBase58(),
+					instructionData: instruction.data.toString("hex"),
+				});
+
+				return {
+					name: "unknown",
+					programId: instruction.programId,
+					args: { unknown: instruction.data },
+					accounts: instruction.keys,
+				} as UnknownInstruction;
+			}
 		}
 	}
 
